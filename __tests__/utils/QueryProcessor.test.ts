a65f6c90fa7d57cfa929ca6ff65a6be776d5c225
ChuyB <bovea3359@gmail.com>
2024-11-15 10:53:23 -0400

    Añade query para retornar el usb-id

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -1,29 +1,26 @@
 import QueryProcessor from "../../utils/QueryProcessor";
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
 
 describe("QueryProcessor", () => {
-    test("should return a string", () => {
-        const query = "test";
-        const response: string = QueryProcessor(query);
-        expect(typeof response).toBe("string");
-    });
+  test("should return a string", () => {
+    const query = "test";
+    const response: string = QueryProcessor(query);
+    expect(typeof response).toBe("string");
+  });
 
-    test('should return shakespeare description', () => {
-        const query = "shakespeare";
-        const response: string = QueryProcessor(query);
-        expect(response).toBe((
-            "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-            "English poet, playwright, and actor, widely regarded as the greatest " +
-            "writer in the English language and the world's pre-eminent dramatist."
-          ));
-    });
+  test("should return shakespeare description", () => {
+    const query = "shakespeare";
+    const response: string = QueryProcessor(query);
+    expect(response).toBe(
+      "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+        "English poet, playwright, and actor, widely regarded as the greatest " +
+        "writer in the English language and the world's pre-eminent dramatist.",
+    );
+  });
 
-    // TODO: Deberías actualizar la prueba a continuación después de añadir tu USB ID
-    test('should return my USB ID', () => {
-        const query = "what's your USB ID?";
-        const response: string = QueryProcessor(query);
-        expect(response).toBe((
-		"YOUR USB ID SHOULD BE HERE"
-    	));
-    });
+  test("should return my USB ID", () => {
+    const query = "what's your USB ID?";
+    const response: string = QueryProcessor(query);
+    expect(response).toBe("19-10072");
+  });
 });
